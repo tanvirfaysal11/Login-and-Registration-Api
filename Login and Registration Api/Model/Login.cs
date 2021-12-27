28aa0e3710e0cec03730221f7b66e8b78d6b6d43
Tanvir Faysal <62283249+tanvirfaysal11@users.noreply.github.com>
2021-12-27 11:03:49 +0600

    Add project files.

---
+++ Login and Registration Api/Model/Login.cs
@@ -0,0 +1,10 @@
+﻿namespace Login_and_Registration_Api.Model
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = String.Empty;
+
+    }
+}
---
+++ Login and Registration Api/Model/Registration.cs
@@ -0,0 +1,9 @@
+﻿namespace Login_and_Registration_Api.Model
+{
+    public class Registration
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+}
